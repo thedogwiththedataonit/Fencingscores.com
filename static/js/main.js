2686956d39a3421c5ba8f3180e53c1ce900282f4
Junseo Park <parkaph@bc.edu>
2021-09-23 19:43:20 -0400

    completed the loaders

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,8 @@ function showloader() {
     document.getElementById('loader').style.display = "flex";
 }
 
+function flashMessage() {
+    if ("{{ flash_message }}" == "True") {
+      alert("No Name matches in Database");
+    }
+  }
