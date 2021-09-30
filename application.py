5b5911ba91cfb4afc78ee45b8441bf9d1e4732f8
Junseo Park <parkaph@bc.edu>
2021-09-30 03:02:51 -0400

    fixed databases

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, request
 from werkzeug.utils import redirect
-import pymongo 
+import pymongo
 from pymongo import MongoClient
 import certifi
+import json
 
 from queryfunctions import query_results
 
 
 application = Flask(__name__)
 
-#list all names
+# list all names
 """"
 cluster = MongoClient("mongodb+srv://thomas:0806@cluster0.2kcsq.mongodb.net/python?retryWrites=true&w=majority", tlsCAFile=certifi.where()) #MAC LINE
     db = cluster["Names_Cluster"]
@@ -19,70 +20,77 @@ cluster = MongoClient("mongodb+srv://thomas:0806@cluster0.2kcsq.mongodb.net/pyth
 """
 
 
-
-@application.route('/', methods=["POST","GET"])
+@application.route('/', methods=["POST", "GET"])
 def main():
-    if request.method == "POST": #IMPROVE SEARCH FUNCTION
+    if request.method == "POST":  # IMPROVE SEARCH FUNCTION
         user = request.form["nm"]
+        #user1 = request.form["nm1"]
 
-        cluster = MongoClient("mongodb+srv://thomas:0806@cluster0.2kcsq.mongodb.net/python?retryWrites=true&w=majority", tlsCAFile=certifi.where()) #MAC LINE
+        cluster = MongoClient("mongodb+srv://thomas:0806@cluster0.2kcsq.mongodb.net/python?retryWrites=true&w=majority", tlsCAFile=certifi.where())  # MAC LINE
         db = cluster["Names_Cluster"]
         collection = db["Names"]
         cap_usr = user.upper()
 
-        
         try:
-            name_document = collection.find_one({"Name":cap_usr})
-            win_pre = (name_document.get('Pool_win_percentage') ) #test to see if name is valid by querying win percentage
-            
+            name_document = collection.find_one({"Name": cap_usr})
+            # test to see if name is valid by querying win percentage
+            win_pre = (name_document.get('Pool_win_percentage'))
+
             return redirect(url_for("user", usr=cap_usr))
         except:
-            flash_message="True"
+            flash_message = "True"
             return render_template('index.html', flash_message=flash_message)
 
     else:
-        flash_message="True"
-        return render_template('index.html', flash_message=flash_message)
+        return render_template('index.html')
 
 
-
-@application.route("/<usr>/", methods=["POST","GET"]) #HOW TO CONTINUE URLS / / /
+# HOW TO CONTINUE URLS / / /
+@application.route("/<usr>/", methods=["POST", "GET"])
 def user(usr):
     results = query_results(usr, "all")
     message = "Alpha Version 1.914"
-    return render_template('profile.html', name=usr, results=results, message=message)
-                            
-@application.route("/<usr>/y14", methods=["POST","GET"]) #NO y14 DATA
+    #json_results = json.dumps(results) #Sending data to js functions for d3.js
+    #print(json_results)
+    return render_template('profile.html', name=usr, results=results, message=message) #json_results=json_results
+
+
+@application.route("/<usr>/y14", methods=["POST", "GET"])  # NO y14 DATA
 def y14(usr):
     results = query_results(usr, "y14")
     message = "Alpha Version 1.914"
     return render_template('y14.html', name=usr, results=results, message=message)
 
-@application.route("/<usr>/cadet", methods=["POST","GET"])
+
+@application.route("/<usr>/cadet", methods=["POST", "GET"])
 def cadet(usr):
     results = query_results(usr, "cadet")
     message = "Alpha Version 1.914"
     return render_template('cadet.html', name=usr, results=results, message=message)
 
-@application.route("/<usr>/junior", methods=["POST","GET"])
+
+@application.route("/<usr>/junior", methods=["POST", "GET"])
 def junior(usr):
     results = query_results(usr, "junior")
     message = "Alpha Version 1.914"
     return render_template('junior.html', name=usr, results=results, message=message)
 
-@application.route("/<usr>/div1", methods=["POST","GET"])
+
+@application.route("/<usr>/div1", methods=["POST", "GET"])
 def div1(usr):
     results = query_results(usr, "div1")
     message = "Alpha Version 1.914"
     return render_template('div1.html', name=usr, results=results, message=message)
 
-@application.route("/<usr>/div2", methods=["POST","GET"])   #NO DIV2 DATA
+
+@application.route("/<usr>/div2", methods=["POST", "GET"])  # NO DIV2 DATA
 def div2(usr):
     results = query_results(usr, "div2")
     message = "Alpha Version 1.914"
     return render_template('div2.html', name=usr, results=results, message=message)
 
-@application.route("/<usr>/div3", methods=["POST","GET"])   #NO DIV3 DATA
+
+@application.route("/<usr>/div3", methods=["POST", "GET"])  # NO DIV3 DATA
 def div3(usr):
     results = query_results(usr, "div3")
     message = "Alpha Version 1.914"
@@ -90,4 +98,4 @@ def div3(usr):
 
 
 if __name__ == "__main__":
-    application.run(debug=True) #turn debug off for prodcution deployment
+    application.run(debug=True)  # turn debug off for prodcution deployment
--- queryfunctions.py
+++ queryfunctions.py
@@ -23,12 +23,13 @@ def query_results(usr, cat):
     #print(collection.distinct("Name"))
 
     name_document = collection.find_one({"Name":usr})
-    win_percentage = name_document.get('Pool_win_percentage')
+    win_percentage = (name_document.get('Pool_win_percentage') * 100) 
     
     
     attended_comps = name_document.get('Competitions')
     attended_sorted_comps = sorted(attended_comps, key=lambda x: int(x.split(", ")[-1]))
     attended_cats = name_document.get("Category")
+    
 
     club = name_document.get("Club")
     nation = name_document.get("Nation")
@@ -40,7 +41,16 @@ def query_results(usr, cat):
     div2 = []
     div3 = []
 
+    list_weapons = []
+
     for category in attended_cats:
+        if category[-1] == "E":
+            list_weapons.append("Epee")
+        if category[-1] == "F":
+            list_weapons.append("Foil")
+        if category[-1] == "S":
+            list_weapons.append("Saber")
+
         #print(category)
         db_comp = cluster[category]
         collection_names_in_category = list(db_comp.list_collection_names())
@@ -54,9 +64,8 @@ def query_results(usr, cat):
 
                     #print(comp)
                     fencer_document = collection_comp.find_one({"Name":usr})
-                    print(fencer_document)
-                   
-                    print("___________________________")
+                    #print(fencer_document)
+                    #print("___________________________")
 
                     all_comps.append(fencer_document)
                     cadet.append(fencer_document)
@@ -68,8 +77,8 @@ def query_results(usr, cat):
                     collection_comp = db_comp[comp] #try... checks if the competition exists within the cluster
                     #print(comp)
                     fencer_document = collection_comp.find_one({"Name":usr})
-                    print(fencer_document)
-                    print("___________________________")
+                    #print(fencer_document)
+                    #print("___________________________")
 
                     all_comps.append(fencer_document)
                     junior.append(fencer_document)
@@ -81,8 +90,8 @@ def query_results(usr, cat):
 
                     #print(comp)
                     fencer_document = collection_comp.find_one({"Name":usr})
-                    print(fencer_document)
-                    print("___________________________")
+                    #print(fencer_document)
+                    #print("___________________________")
 
                     all_comps.append(fencer_document)
                     div1.append(fencer_document)
@@ -91,6 +100,7 @@ def query_results(usr, cat):
                 continue
     #print(all_comps)
     
+    list_weapons = list(dict.fromkeys(list_weapons))
     if (cat == "all"): #WTF THE TITLES DONT LINE UP BUG BUG BUG
         return (usr, 
                 win_percentage, 
@@ -99,6 +109,7 @@ def query_results(usr, cat):
                 nation,
                                 
                 all_comps,
+                list_weapons,
                 )   
     if cat == "cadet":
         return (usr, 
@@ -108,6 +119,7 @@ def query_results(usr, cat):
                 nation,
 
                 cadet,
+                list_weapons,
                 )   
     if cat == "junior":
         return (usr, 
@@ -117,6 +129,7 @@ def query_results(usr, cat):
                 nation,
                 
                 junior,
+                list_weapons,
                 )
     if cat == "div1":
         return (usr,                        #0
@@ -126,19 +139,22 @@ def query_results(usr, cat):
                 nation,                     #4
 
                 div1,                       #5
+                list_weapons,
                 ) 
+
+    holder_variable = "NA"
     if (cat == "y14") or (cat == "div2") or (cat == "div3"):
         return (usr,                        #0
                 win_percentage,             #1 
                 club,                       #2
                 len(club),                  #3
                 nation,                     #4
-
-                             #5
+                holder_variable,            #5
+                list_weapons,             
                 )
 
 
-#results = (query_results("PARK THOMAS JUNSEO", "all"))
+#results = (query_results("PARK, THOMAS JUNSEO", "all"))
 #print(results)
 #print(len(results[5]["win_fencer_len"]))
 #print(len(results[5]["win_score_list"]))
